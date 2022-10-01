fe826e63717d153c03d07fd8e7f2b4a58c890206
Chaimae Daoudi <chaimaadaoudi1908@gmail.com>
2022-10-01 13:37:55 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+constructor(props) {
+    super(props)
+    this.state = {
+      fullName: 'chaimae daoudi',
+      b: true
+    }
+  }
+  
+
+
+render() {
+  if(this.state.b){
+	return (
+	<div >
+		<p>{this.state.fullName} </p>
+
+		<button onClick={	()=>this.setState({ b: !this.state.b})
+}>
+		Click me!
+		</button>
+	</div>
+	);
+
+  }
+  else {
+    return (
+    	<button onClick={ ()=>	this.setState({ b: !this.state.b})
+}>
+		Click me!
+		</button>)
+  }
+}
 }
 
 export default App;
